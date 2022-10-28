9c3526046c16c5c9ea3252dcb09ff28e0095351b
lizethdd <keeylic@outlook.es>
2022-10-28 03:01:44 +0200

    DONE

---
+++ src/Genericos/ClaseGenerica.java
@@ -0,0 +1,18 @@
+
+package Genericos;
+
+
+public class ClaseGenerica<T> {
+    private T objeto;
+
+    public ClaseGenerica(T objeto) {
+        this.objeto = objeto;
+    }
+    
+    public void obtenerTipo(){
+        System.out.println("El tipo T es: "+objeto.getClass().getSimpleName());//tipo para esta clase g
+    }
+    
+    
+    
+}
